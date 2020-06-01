642c47d6ffe76bff5eb495689e4ec66a0626c6d8
Leandro W. Konopka <gamesle@hotmail.com>
2020-06-01 13:08:02 -0300

    Commit Service and Repository Interfaces, CoursesViewModels

---
+++ Clean_Arch/ClearArch.Domain/Interfaces/ICourseRespository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//ing ClearArch.Domain.Models;
+
+namespace ClearArch.Domain.Interfaces
+{
+    public interface ICourseRespository
+    {   
+        IEnumerable<Course> GetCourses();
+    }
+}
