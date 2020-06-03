7084af7f6e39f6a1f33a46f52d64d837849d7d0e
Leandro W. Konopka <gamesle@hotmail.com>
2020-06-03 11:51:45 -0300

    Configure Presentation Layer for Dependency Container

---
+++ Clean_Arch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Infra.Data.Repository;
+using ClearArch.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Infra.Data Layer
+            services.AddScoped<ICourseRespository, CourseRepository>();
+        }
+
+
+    }
+}
--- Clean_Arch/CleanArch/Startup.cs
+++ Clean_Arch/CleanArch/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 
 namespace CleanArch
 {
@@ -41,6 +42,8 @@ namespace CleanArch
                   options.UseSqlServer(
                      Configuration.GetConnectionString("UniversityDBConnection"));
               });
+
+            RegisterService(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +75,10 @@ namespace CleanArch
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+        private static void RegisterService(IServiceCollection services)
+        {
+            DependencyContainer.RegisterService(services);
         }
     }
 }
